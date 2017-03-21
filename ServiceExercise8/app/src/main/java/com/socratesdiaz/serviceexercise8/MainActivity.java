5673cc251437c313e3a382bc66996b29a13e32dc
Socrates Diaz <s.diaz@caribemedia.com.do>
2017-03-21 18:04:07 -0400

    Added runtime permissions to ServiceExercise

--- ServiceExercise8/app/src/main/java/com/socratesdiaz/serviceexercise8/MainActivity.java
+++ ServiceExercise8/app/src/main/java/com/socratesdiaz/serviceexercise8/MainActivity.java
@@ -1,19 +1,29 @@
 package com.socratesdiaz.serviceexercise8;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.DownloadListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int DOWNLOAD_SERVICE_REQUEST_CODE = 1001;
+
     private TextView mTextView;
+    private Button mButton;
+
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -38,6 +48,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.status);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)
+                        && ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                        startDownloadService();
+
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[] { Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                                DOWNLOAD_SERVICE_REQUEST_CODE);
+                    }
+                }
+
+            }
+        });
     }
 
     @Override
@@ -52,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         this.unregisterReceiver(mReceiver);
     }
 
-    public void onClick(View view) {
+    public void startDownloadService() {
 
         Intent intent = new Intent(this, DownloadService.class);
         // add infos for the service which file to download and where to store
@@ -60,5 +94,21 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(DownloadService.URL, "http://www.vogella.com/index.html");
         startService(intent);
         mTextView.setText("Service started");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case DOWNLOAD_SERVICE_REQUEST_CODE: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startDownloadService();
+                } else {
+                    Toast.makeText(
+                            MainActivity.this, "Couldn't start service", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
